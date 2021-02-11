6840a5e28ae1cce58761a9918d911167b08b95a3
ciroesteves <73673247+ciroesteves@users.noreply.github.com>
2021-02-11 19:23:24 -0300

    Add files via upload

---
+++ Java_ativ1/Scripts/jscript.js
@@ -0,0 +1,31 @@
+function funTrocaTema(par_tema) {
+    CSS_tema.href = "Estilo/CSS_" + par_tema + ".css";
+    texto_destaque.innerHTML = par_tema;
+    img_destaque.src = "Imagens/" + par_tema + ".png";
+    img_destaque.alt = img_destaque.tittle = par_tema;
+}
+function funBorda() {
+    if (borda.checked) {
+        cor.disabled = false;
+        alert("Agora selecione uma cor!")
+    }
+    else {
+        texto_destaque.style.borderStyle = "none";
+        cor.options[0].selected = "selected";
+        cor.disabled = true
+    }
+}
+function funCorBorda() {
+    texto_destaque.style.borderStyle = "solid";
+    texto_destaque.style.borderColor = cor.value;
+}
+function funSombra() {
+    if (sombra.value == "Adiciona Sombra") {
+        texto_destaque.className = "sombra_texto";
+        sombra.value = "Remove Sombra";
+    }
+    else {
+        texto_destaque.className = "";
+        sombra.value = "Adiciona Sombra";
+    }
+}
